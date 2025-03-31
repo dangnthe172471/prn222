7474c019a86657f762d94b0620c24aaa35c36b38
huanndhe172325 <Huanndhe172325@fpt.edu.vn>
2025-04-01 02:25:01 +0700

    update

--- Project_PRN222/Pages/Login/ForgetPassword.cshtml.cs
+++ Project_PRN222/Pages/Login/ForgetPassword.cshtml.cs
@@ -1,12 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json.Linq;
 
 namespace Project_PRN222.Pages.Login
 {
     public class ForgetPasswordModel : PageModel
     {
-        public void OnGet()
-        {
-        }
+
+
+        [BindProperty]
+        public string Email { get; set; }
+
+        [BindProperty]
+        public string RecaptchaResponse { get; set; }
+
+
+
     }
 }
